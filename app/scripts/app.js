f9a7d8591ad36243704f5d8bc9679518cb3a1b2e
dmaring <andrew.maring@gmail.com>
2017-08-02 15:19:33 -0700

    completed checkpoint 2

---
+++ app/scripts/app.js
@@ -0,0 +1,3 @@
+// defines the angular.module for this app
+angular.module('blocJams', []);
+/* The first argument passed, blocJams, is the prescribed name of the module. The empty array, passed as the second argument, injects dependencies into an application. For now, there are no dependencies to inject.*/
