fd9e794260dbf7307f5a3ad94b80dda4b8d53ccf
dmaring <andrew.maring@gmail.com>
2017-08-23 10:58:01 -0700

    completed checkpoint 10

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         /**
         * @desc SongPlayer object to be returned with its public properties and methods
         * @type {Object}
@@ -42,6 +42,12 @@
                 preload: true,
             });
 
+            currentBuzzObject.bind('timeupdate', function() {
+              $rootScope.$apply(function() {
+                SongPlayer.currentTime = currentBuzzObject.getTime();
+              });
+            });
+
             SongPlayer.currentSong = song;
         }
         /**
@@ -71,6 +77,24 @@
         * @type {String}
         */
         SongPlayer.currentSong = null;
+
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
+
+
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function(time){
+          if (currentBuzzObject) {
+            currentBuzzObject.setTime(time);
+          }
+        };
 
         /**
         * @function SongPlayer.play
@@ -146,5 +170,5 @@
     }
     angular
         .module('blocJams')
-        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
